aa5949557b0ba8e4155ae37c5b0de2856d678115
LucasVeloz <lurucell@gmail.com>
2021-10-24 18:13:24 -0300

    feat: player

--- App.tsx
+++ App.tsx
@@ -1,21 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import { Video, Audio } from 'expo-av';
+import { TapGestureHandler } from 'react-native-gesture-handler'
+import Animated, { useAnimatedStyle, useSharedValue, withDelay, withSpring, withTiming } from 'react-native-reanimated';
+import { AntDesign } from '@expo/vector-icons';
+
+const videoURL2 = 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4'
+const { height, width } = Dimensions.get('window');
 
 export default function App() {
+  const AnimatedIcon = Animated.createAnimatedComponent(AntDesign);
+  const video = useRef<Video>(null);
+  const doubleTapRef = useRef();
+  const [teste, setTeste] = useState(true);
+  const scale = useSharedValue(0);
+  const scalePlayAndPause = useSharedValue(0);
+
+  const functionTeste = useCallback(() => {
+    setTeste(oldState => !oldState);
+    scalePlayAndPause.value = withSpring(1, undefined, (isFinished) => {
+      if (isFinished) {
+        scalePlayAndPause.value = withDelay(300,withTiming(0));
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    Audio.setAudioModeAsync({
+      playsInSilentModeIOS: true,
+    })
+  }, [])
+
+  const doubleTap = useCallback(() => {
+    scale.value = withSpring(1, undefined, (isFinished) => {
+        if (isFinished) {
+          scale.value = withDelay(300,withTiming(0));
+        }
+      });
+  }, []);
+
+  const rStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: scale.value,
+      }
+    ] 
+  }))
+
+  const playAndPauseStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: scalePlayAndPause.value,
+      }
+    ] 
+  }))
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <TapGestureHandler 
+        waitFor={doubleTapRef} 
+        onActivated={functionTeste}
+        >
+        <TapGestureHandler maxDelayMs={250} ref={doubleTapRef} numberOfTaps={2} onActivated={doubleTap}>
+          <Animated.View>
+            <Video
+              ref={video}
+              style={styles.video}
+              source={{
+                uri: videoURL2
+              }}
+              resizeMode="cover"
+              isLooping
+              shouldPlay={teste}
+              />
+            {
+              teste ?
+              <AnimatedIcon name="playcircleo" style={[styles.icon, playAndPauseStyle]} />
+              :
+              <AnimatedIcon name="pausecircle" style={[styles.icon, playAndPauseStyle]} />
+            }
+            <AnimatedIcon name="heart" style={[styles.icon, rStyle]} />
+          </Animated.View>
+        </TapGestureHandler>
+      </TapGestureHandler>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  video: {
+    height,
+    width,
   },
+  icon: {
+    position: 'absolute',
+    color: 'white',
+    alignSelf: 'center',
+    top: '50%',
+    fontSize: 100,
+  }
 });
